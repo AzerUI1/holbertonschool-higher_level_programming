ee19990c5cefbf743cda85696cd7df9d5202c95d
AzerUI1 <azer0808aslanov@gmail.com>
2025-10-21 13:03:18 +0000

    py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):  # loop for the first digit
-    for j in range(i + 1, 10):  # loop for the second digit, always greater than first
-        if i == 8 and j == 9:  # last combination
-            print("{}{}".format(i, j))  # print without comma and space
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
         else:
-            print("{}{}, ".format(i, j), end="")  # print with comma and space
+            print("{}{}, ".format(i, j), end="")
