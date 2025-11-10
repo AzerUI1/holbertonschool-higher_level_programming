6d85b9d8dba5fa2af3c7dfb05cad2f0cca701df7
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 11:57:51 +0400

    Refine docstring in read_file function
    Updated docstring for clarity and consistency.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+'''This module defines a function to read and print the content of a text file.'''
 
 def read_file(filename=""):
-    """Reads a text file and prints its contents to stdout.
-
-    Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
-    """
+    """Reads a text file and prints its content to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         try:
             print(content, end='')
-        finally: 
+        finally:
             file.close()
