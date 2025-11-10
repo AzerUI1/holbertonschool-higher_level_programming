08e4f47366ae5122a55bef460868cadd6323ed21
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 12:05:58 +0400

    Fix file closing in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module that defines a function to write the content of a text file.'''
 
+
 def write_file(filename="", text=""):
     '''Writes a text into file'''
     with open(filename, 'w', encoding='utf-8') as txt_file:
