56b3933a5d979b97f7571d732fc2f59555011932
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 12:56:41 +0400

    Create Student class with initialization and to_json method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Write a class Student that defines a student'''
+
+
+class Student: 
+    '''Defines a student by: (based on 10-student.py)'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self, attrs=None):
+        '''Retrieve a dictionary representation of a Student instance'''
+        return self.__dict__
