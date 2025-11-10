1bae564594f9923090bfafbc8cd12aee1f026ba9
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 12:05:11 +0400

    Add write_file function to write text to a file
    Defines a function to write text to a file with UTF-8 encoding.

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module that defines a function to write the content of a text file.'''
+
+def write_file(filename="", text=""):
+    '''Writes a text into file'''
+    with open(filename, 'w', encoding='utf-8') as txt_file:
+        content = txt_file.write(text)
+        try:
+            return content 
+        finally:
+            txt_file.close() 
