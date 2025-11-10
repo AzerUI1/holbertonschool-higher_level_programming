1d2b352eab9644cadc762a04bb457810ff8cca23
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 12:00:36 +0400

    Fix docstring formatting in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''This module defines a function to read and print the content of a text file.'''
+'''Module that defines a function to read the content of a text file.'''
+
 
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout."""
+    '''Reads a text file and prints its content to stdout.'''
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         try:
