6d8401c3edc391ef52911e7ff0998e716843c47b
Azar Aslanov <azer0808aslanov@gmail.com>
2025-11-10 12:52:19 +0400

    Fix docstring in class_to_json function
    Updated docstring to clarify function purpose.

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Write a function that returns the dictionary description with simple data structure'''
+'''Write a function that returns the dictionary description'''
 
 
 def class_to_json(obj):
